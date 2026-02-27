f5ccfdcb88045fa6a34c6be0d58139f9ea694f76
pavanmagadum <pavanmagadum8@gmail.com>
2026-02-27 23:14:20 +0530

    Added an react to the chatbot project to make an ui asthatic

---
+++ react_project/src/Components/Chat.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+function Chat() {
+  return (
+    <div>
+      <form>
+        <input type = "text" placeholder="Enter your name" /><br /><br />
+        <input type = "text" placeholder="Enter your USN" />
+
+      </form>
+    </div>
+  )
+}
+
+export default Chat
